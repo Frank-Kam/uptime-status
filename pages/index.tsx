4175608018ec12318ecb93ce5d6da3082f8fce44
Frank <ganl68384@gmail.com>
2025-01-27 17:11:58 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,7 +80,7 @@ export default function Home({
           <a href="mailto:mail@frankk.uk" target="_blank">
             Contact Me
           </a>
-        </text>
+        </Text>
         
       </main>
     </>
